e4259da1680cc685d1492327a8c2461c8a77ed46
gyeoul <clsmay.1st@gmail.com>
2023-04-11 14:10:41 +0900

    [BOJ | Kotlin] 2580
    Signed-off-by: gyeoul <clsmay.1st@gmail.com>

--- baekjoon/bruteforce/_2580/gyeoul_2580.kt
+++ baekjoon/bruteforce/_2580/gyeoul_2580.kt
@@ -5,68 +5,48 @@ import java.io.BufferedWriter
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
 import java.util.*
-import kotlin.collections.ArrayList
 
-/*
-0 3 5 4 6 9 2 7 8
-7 8 2 1 0 5 6 0 9
-0 6 0 2 7 8 1 3 5
-3 2 1 0 4 6 8 9 7
-8 0 4 9 1 3 5 0 6
-5 9 6 8 2 0 4 1 3
-9 1 7 6 5 2 0 8 0
-6 0 3 7 0 1 9 5 2
-2 5 8 3 9 4 7 6 0
-
-메모 배열 생성시
-제작해야할 함수
-1. 채워진 정보를 바탕으로 메모 수정
-2. 메모에서 값을 체크하고 1,2 개일때 각각 로직 작성
-3. 2일경우 백트래킹 고려
- */
-fun get3by3(a: Int, b: Int, arr: ArrayList<ArrayList<Int>>): TreeSet<Int> {
-    val res = TreeSet<Int>()
-    for (i in 0..2) {
-        for (j in 0..2) {
-            if (arr[i + (3 * (a / 3))][j + (3 * (b / 3))] != 0)
-                res.add(arr[i + (3 * (a / 3))][j + (3 * (b / 3))])
+fun calculate(arr: ArrayList<ArrayList<Int>>): Boolean {
+    for (i in 0..8) {
+        for (j in 0..8) {
+            if (arr[i][j] != 0) continue
+            for (k in 1..9) {
+                if (!isAvailable(arr, Pair(i, j), k)) continue
+                arr[i][j] = k
+                if (calculate(arr)) return true
+                arr[i][j] = 0
+            }
+            return false
         }
     }
-    return res
+    return true
+}
+
+fun isAvailable(arr: ArrayList<ArrayList<Int>>, loc: Pair<Int, Int>, num: Int): Boolean {
+    for (i in 0..8) {
+        if (arr[loc.first][i] == num || arr[i][loc.second] == num) return false
+        if (arr[(loc.first / 3) * 3 + i / 3][(loc.second / 3) * 3 + i % 3] == num) return false
+    }
+    return true
 }
 
 fun main() {
     val bf = BufferedReader(InputStreamReader(System.`in`))
     val bw = BufferedWriter(OutputStreamWriter(System.out))
-    val q: Queue<Pair<Int, Int>> = LinkedList()
-
     val arr = ArrayList<ArrayList<Int>>()
 
-    for (i in 1..9) {
+    for (i in 0..8) {
         val st = StringTokenizer(bf.readLine())
         arr.add(ArrayList())
-        for (j in 1..9) {
-            val n = st.nextToken().toInt()
-            if (n == 0) q.add(Pair(i - 1, j - 1))
-            arr.last().add(n)
+        for (j in 0..8) {
+            val now = st.nextToken().toInt()
+            arr.last().add(now)
         }
     }
-    while (q.isNotEmpty()) {
-        val now = q.remove()
-        val has = TreeSet<Int>()
-        val nums = (1..9).toList() as ArrayList<Int>
-        has.addAll(get3by3(now.first, now.second, arr))
-        for (i in 0..8) {
-            has.add(arr[now.first][i])
-            has.add(arr[i][now.second])
-        }
-        nums.removeAll(has)
-        if (nums.size==1)arr[now.first][now.second] = nums[0]
-        else q.offer(Pair(now.first, now.second))
-    }
+    calculate(arr)
 
     for (v in arr) {
-        for (w in v){
+        for (w in v) {
             bw.write("$w ")
         }
         bw.write("\n")
