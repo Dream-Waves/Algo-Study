d1e9a6024de09eccdc909f091adabc6b5470011d
gyeoul <clsmay.1st@gmail.com>
2023-04-12 14:43:40 +0900

    [BOJ | Kotlin] 1987
    Signed-off-by: gyeoul <clsmay.1st@gmail.com>

--- baekjoon/bruteforce/_1987/gyeoul_1987.kt
+++ baekjoon/bruteforce/_1987/gyeoul_1987.kt
@@ -9,29 +9,20 @@ import java.util.*
 val dy = intArrayOf(1, 0, -1, 0)
 val dx = intArrayOf(0, 1, 0, -1)
 var res = 1
-lateinit var isVisited: Array<BooleanArray>
-fun run(arr: Array<IntArray>, step: HashSet<Int>, count: Int, next: Pair<Int, Int>) {
-    if (step.contains(arr[next.first][next.second])) {
-//        if (count >= 26) {
-//            res = 26
-//            return
-//        }
-//        println(step)
+lateinit var arr: Array<IntArray>
+
+fun run(step: HashSet<Int>, count: Int, now: Pair<Int, Int>) {
+    if (now.first !in arr.indices || now.second !in arr[now.first].indices || step.contains(arr[now.first][now.second])) {
         if (count > res) res = count
         return
     }
-    step.add(arr[next.first][next.second])
+    step.add(arr[now.first][now.second])
     for (i in 0..3) {
-        val newy = dy[i] + next.first
-        val newx = dx[i] + next.second
-        if (newy !in 0 until arr.size) continue
-        if (newx !in 0 until arr[newy].size) continue
-        if (isVisited[newy][newx]) continue
-        isVisited[newy][newx] = true
-        run(arr, step, count + 1, Pair(newy, newx))
-        isVisited[newy][newx] = false
+        val nexty = dy[i] + now.first
+        val nextx = dx[i] + now.second
+        run(step, count + 1, Pair(nexty, nextx))
     }
-    step.remove(arr[next.first][next.second])
+    step.remove(arr[now.first][now.second])
 }
 
 fun main() {
@@ -41,31 +32,14 @@ fun main() {
     val st = StringTokenizer(bf.readLine())
     val y = st.nextToken().toInt()
     val x = st.nextToken().toInt()
-    val arr = Array(y) { IntArray(x) }//ArrayList<ArrayList<Int>>()
-//    val stack = Stack<>
-//    val ss = HashSet<Int>()
-//    ss.contains()
-
-    isVisited = Array(y) { BooleanArray(x) }
-
+    arr = Array(y) { IntArray(x) }
     for (i in 0 until y) {
         val now = bf.readLine().toCharArray()
-//        arr.add(ArrayList())
         for (j in 0 until x) {
-//            arr.last().add(now[j] - 'A')
             arr[i][j] = now[j] - 'A'
         }
     }
-
-    run(arr, step, 0, Pair(0, 0))
-
-//    for (v in arr) {
-//        for (w in v) {
-//            bw.write("${w / 10}${w % 10} ")
-//        }
-//        bw.write("\n")
-//    }
-//    if (res < 1) res = 1
+    run(step, 0, Pair(0, 0))
     bw.write("$res")
     bw.flush()
 }
